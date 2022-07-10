f68027c671bbe359c84cb82d303f645e979251a2
Freddy Guapi <frddgpmll6@gmail.com>
2022-07-10 15:11:13 -0500

    Error Handling

--- controllers/news.controllers.js
+++ controllers/news.controllers.js
@@ -1,39 +1,69 @@
 import News from "../models/news.model.js";
 
 export const getNews = async (req, res) => {
-  const news = await News.find();
-  res.json(news);
+  try {
+    const news = await News.find();
+    res.json(news);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 export const getNew = async (req, res) => {
-  const news = await News.findById(req.params.id);
-  if (!news)
-    return res.status(404).json({
-      message: "News does not exists",
+  try {
+    const news = await News.findById(req.params.id);
+    if (!news)
+      return res.status(404).json({
+        message: "News does not exists",
+      });
+    return res.json(news);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
     });
-  return res.json(news);
+  }
 };
 export const createNews = async (req, res) => {
-  const { author, title, content } = req.body;
-  const news = new News({
-    author,
-    title,
-    content,
-  });
-  await news.save();
-  res.json(news);
+  try {
+    const { author, title, content } = req.body;
+    const news = new News({
+      author,
+      title,
+      content,
+    });
+    await news.save();
+    res.json(news);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 export const updateNews = async (req, res) => {
-  const { id } = req.params;
-  const newsUpdated = await News.findByIdAndUpdate(id, req.body, {
-    new: true,
-  });
-  return res.json(newsUpdated);
+  try {
+    const { id } = req.params;
+    const newsUpdated = await News.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    return res.json(newsUpdated);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 export const deleteNews = async (req, res) => {
-  const news = await News.findByIdAndDelete(req.params.id);
-  if (!news)
-    return res.status(404).json({
-      message: "News does not exists",
+  try {
+    const news = await News.findByIdAndDelete(req.params.id);
+    if (!news)
+      return res.status(404).json({
+        message: "News does not exists",
+      });
+    return res.json(news);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
     });
-  return res.json(news);
+  }
 };
