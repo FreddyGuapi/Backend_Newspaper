eb48ca818253b1a099e8c65a6cab3132dd77e03f
Freddy Guapi <frddgpmll6@gmail.com>
2022-07-09 23:10:19 -0500

    Model and Create, Read

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(cors())
 app.use(morgan('dev'))
+app.use(express.json())
 
 app.use(indexRoutes)
 app.use(newsRouters)
--- controllers/news.controllers.js
+++ controllers/news.controllers.js
@@ -1,5 +1,19 @@
+import News from '../models/news.model.js';
 
-export const getNews = (req, res) => res.send('Getting News');
-export const createNews = (req, res) => res.send('Creating News');
+export const getNews = async (req, res) => {
+    const news = await News.find()
+    res.json(news)
+};
+export const createNews =  async (req, res) => {
+    const {author, title, content} = req.body
+    
+    const news = new News({
+        author,
+        title,
+        content
+    })
+    await news.save()
+    res.json(news)
+};
 export const updateNews = (req, res) => res.send('Updating News');
 export const deleteNews = (req, res) => res.send('Delete News');
